e6516d31043de3f164b504a63d49eb5c251d0619
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-04 15:05:57 +0530

    Add files via upload

---
+++ jaya1.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+as_string="programmer"
+print len(as_string)
+
