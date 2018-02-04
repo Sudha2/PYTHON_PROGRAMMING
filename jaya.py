eae73a81385f0aa5a0c5747b4d823ebb3cf735f9
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-04 14:39:29 +0530

    Add files via upload

---
+++ jaya.py
@@ -0,0 +1,31 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=input()
+b=input()
+c=input()
+d=input()
+e=input()
+f=input()
+g=input()
+h=input()
+i=input()
+j=input()
+print(a+b+c+d+e+f+g+h+i+j)
+print(a-b-c-d-e-f-g-h-i-j)
+print(a*b*c*d*e*f*g*h*i*j)
+print(a/b/c/d/e/f/g/h/i/j)
+print(a%b%c%d%e%f%g%h%i%j)
