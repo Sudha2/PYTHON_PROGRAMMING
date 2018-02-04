476aab4b0e251bd6bc8fba9452f78aa6dff9882e
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-04 15:14:45 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      student
+#
+# Created:     04/02/2018
+# Copyright:   (c) student 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=input()
+if(1<a<100000):
+    print("positive")
+else:
+    print("negative")
+
+
