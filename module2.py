bbd715f485510a50e278ff495860572b400c6937
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-05 23:14:39 +0530

    Add files via upload

---
+++ module2.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ELCOT
+#
+# Created:     05-02-2018
+# Copyright:   (c) ELCOT 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+mystring='programmer'
+print(mystring.find("r"))
+print(mystring[3:4])
+print(mystring.replace('m','y'))
+
+
