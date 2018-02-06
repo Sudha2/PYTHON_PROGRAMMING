467e6f94cf058cf2c30f42d56539c9075300268a
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-06 19:33:28 +0530

    Add files via upload

---
+++ module12.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      ELCOT
+#
+# Created:     05-02-2018
+# Copyright:   (c) ELCOT 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=int(input("print the integer:"))
+if(a<0):
+    print("eligible for voting")
+else:
+    print("not eligible")
