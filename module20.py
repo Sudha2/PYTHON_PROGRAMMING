ee9f59f8e7e4b81821e602b8f0657f9566d95093
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-07 22:09:01 +0530

    Add files via upload

---
+++ module20.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ELCOT
+#
+# Created:     07-02-2018
+# Copyright:   (c) ELCOT 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+for hello in range(0,5):
+    print("hello")
