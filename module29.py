698fbcf042771409b95d19601ceac70d7be4d461
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-09 22:06:25 +0530

    Add files via upload

---
+++ module29.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ELCOT
+#
+# Created:     09-02-2018
+# Copyright:   (c) ELCOT 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+chr=input('')
+if((chr>='a' and chr<='z')or(chr>='A'and chr<='Z')):
+          print("Alphabet")
+else:
+    print("No alphabet")
+
