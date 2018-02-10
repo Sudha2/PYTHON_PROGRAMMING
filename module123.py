4168c0f30631eccedf901b83b120e19d388db982
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-10 20:59:20 +0530

    Add files via upload

---
+++ module123.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ELCOT
+#
+# Created:     10-02-2018
+# Copyright:   (c) ELCOT 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a= int(input("enter the year"))
+if(a%4==0):
+    print("yes")
+else:
+    print("no")
