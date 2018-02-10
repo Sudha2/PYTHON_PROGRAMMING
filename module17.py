7cd4d7be2860750884c9e75840a2c5245266ede5
Sudha2 <36107959+Sudha2@users.noreply.github.com>
2018-02-10 21:08:33 +0530

    Add files via upload

---
+++ module17.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ELCOT
+#
+# Created:     10-02-2018
+# Copyright:   (c) ELCOT 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+str=int(input("enter the no"))
+for str in range(1,3):
+    print("Hello")
